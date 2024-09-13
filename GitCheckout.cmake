4ecf54187bae19a383b3ec51944b1f9450a23c71
Santiago Ospina De Los Rios <sospinar@gmail.com>
2024-09-13 11:39:05 +0200

    Checkout git branch on modules

---
+++ GitCheckout.cmake
@@ -0,0 +1,9 @@
+# Checkout git tracked branch (if exists)
+
+if (BRANCH)
+  find_package(Git REQUIRED)
+
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} -C ${CMAKE_CURRENT_BINARY_DIR} checkout ${BRANCH}
+    COMMAND_ECHO STDOUT)
+endif()
